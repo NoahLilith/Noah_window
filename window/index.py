d3bb8123066b51664896605e36e371f481df6d4d
noahHsiao <hgyk021@gmail.com>
2024-05-28 14:51:24 +0800

    modi

--- window/index.py
+++ window/index.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 def get_names() -> list[str]:
     with open('names.txt',encoding='utf-8') as file:
@@ -7,15 +8,23 @@ def get_names() -> list[str]:
     return names
 
 class Window(tk.Tk):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, title:str = "Hellp! Tkinter!", **kwargs):
+        super().__init__(**kwargs)
         #多做一些事
-        self.title("FIRST GUI")
+        self.title(title)
         self.geometry('500x300')
         self.resizable(1,1)
+        label:ttk.Label = ttk.Label(self, 
+                                    text = "Hello! World!",
+                                    font = ('Arial',20,'bold'),
+                                    foreground = '#f00')
+        label.pack(padx=100,pady=40)
+
         
 if __name__ == '__main__':
     names:list[str] = get_names()
-    window:Window = Window()
+    window:Window = Window(title = "FIRST GUI")
     #window.title("FIRST GUI")
+    CT = tk.Button(text="計數器")
+    CT.pack(padx=100,pady=80)
     window.mainloop()
