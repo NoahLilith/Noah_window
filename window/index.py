41362158cfa62f7dc565ba0d4817e8a8a0285172
noahHsiao <hgyk021@gmail.com>
2024-05-28 13:54:47 +0800

    modi

--- window/index.py
+++ window/index.py
@@ -1,13 +1,19 @@
 import tkinter as tk
 
 def get_names() -> list[str]:
-    with open('names.txt',encoding='utf-8') as file:
-        content:str = file.read()
-    names:list[str] = content.split()
+    #with open('names.txt',encoding='utf-8') as file:
+    #    content:str = file.read()
+    #names:list[str] = content.split()
     return names
 
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        #多做一些事
+        self.title("FIRST GUI")
+        
 if __name__ == '__main__':
     names:list[str] = get_names()
-    window:tk.Tk = tk.Tk()
-    window.title("Python GUI")
+    window:Window = Window()
+    #window.title("FIRST GUI")
     window.mainloop()
