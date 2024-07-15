b5416a0573a1b53ebb7441353bcb58181542f182
noahHsiao <hgyk021@gmail.com>
2024-07-15 10:24:03 +0800

    0711

--- 2024-07-11/main.py
+++ 2024-07-11/main.py
@@ -1,7 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import data
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    content = 'Use our powerful <strong>mobile-first</strong> flexbox grid to build layouts of all shapes and sizes thanks to a twelve column system, six default responsive tiers, Sass variables and mixins, and dozens of predefined classes.'
-    return render_template('index.html.jinja',left=content)
+    #print(list(map(lambda value:value[0],data.get_areas())))
+    selected_area = request.args.get('area')
+    areas = [tup[0] for tup in data.get_areas()]
+    selected_area = '士林區' if selected_area is None else selected_area  
+        
+    return render_template('index.html.jinja',areas=areas,show_area=selected_area)    
+    
+    
+    
