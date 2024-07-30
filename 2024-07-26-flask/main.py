0d816192d86b494c793c7e4ed092b55717a24523
noahHsiao <hgyk021@gmail.com>
2024-07-30 08:58:00 +0800

    upd

--- 2024-07-26-flask/main.py
+++ 2024-07-26-flask/main.py
@@ -1,26 +1,36 @@
 from flask import Flask,render_template,request
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+from dashboard.board1 import app1
+from dashboard.board2 import app2
 import data
+import dashboard
 
 app = Flask(__name__)
+application = DispatcherMiddleware(app,{
+    "/dashboard/app1":app1.server,
+    "/dashboard/app2":app2.server
+})
+
 @app.route("/")
 def index():
-    return render_template("index1.html.jinja")
+    return render_template("index.html.jinja")
 
 @app.route("/index1")
 def index1():
     #print(list(map(lambda value:value[0],data.get_areas())))
     selected_area = request.args.get('area')
     areas = [tup[0] for tup in data.get_areas()]
-    selected_area = '¤hªL°Ï' if selected_area is None else selected_area
+    selected_area = 'å£«æ—å€' if selected_area is None else selected_area
     detail_snaes = data.get_snaOfArea(area=selected_area)
     
-    #areas->©Ò¦³¦æ¬F°Ï 
-    #show_area -> ­nÅã¥Üªº¦æ¬F°Ï
-    #detail_snaes -> ¸Ó¦æ¬F°Ï©Ò¦³¯¸ÂI¸ê°T   
-    return render_template('index.html.jinja',areas=areas,show_area=selected_area,detail_snaes=detail_snaes)    
+    #areas->æ‰€æœ‰è¡Œæ”¿å€ 
+    #show_area -> è¦é¡¯ç¤ºçš„è¡Œæ”¿å€
+    #detail_snaes -> è©²è¡Œæ”¿å€æ‰€æœ‰ç«™é»è³‡è¨Š   
+    return render_template('index1.html.jinja',areas=areas,show_area=selected_area,detail_snaes=detail_snaes)    
     
     
-    
-    
-    
-    
+
+
+if __name__ == "__main__":
+    run_simple("localhost", 8080, application,use_debugger=True,use_reloader=True)
